63bffc36ce0ae5ed47433dde82b63608f879213f
Enrique <okay.enrique@gmail.com>
2022-12-12 17:10:02 -0500

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import beersImg from "../assets/beers.png";
+import newBeerImg from "../assets/new-beer.png";
+import randomBeerImg from "../assets/random-beer.png";
+
+const HomePage = () => {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default HomePage;
